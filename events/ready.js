7bc1a18ea231f02428eedfeba3189ca70488b084
ava <72134649+fadeawaycodes@users.noreply.github.com>
2020-12-18 06:32:50 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = (client) => {
     `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
   );
 
-  const activities = [`People win giveaways in ${client.guilds.cache.size} servers.`,"e!help",`over ${client.users.cache.size} users!`, "Fade away#2777 code me."];
+  const activities = [`Giveaways finish. | e!help`,];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
     client.user.setActivity(activity, { type: "WATCHING" });
